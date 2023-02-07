abae5d79c1f1adbecbed21c7ca52837940a49336
ant-mand <amanduva@uchicago.edu>
2023-02-06 23:04:24 -0600

    files

---
+++ def sum_fuel_reqs(txt):.py
@@ -0,0 +1,10 @@
+def sum_fuel_reqs(txt):
+    """
+    Calculate sum of fuel requirements for each module.
+
+    Inputs:
+        txt[text]: text file
+
+    Returns[int]: sum of fuel requirements
+    """
+    
